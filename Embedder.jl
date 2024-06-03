7a18946260a75fa85a68191d975092f39014af70
comfybytes <comfybytes@mailbox.org>
2024-06-03 20:37:08 +0200

    adjust positional encoding for more than one batch in data

--- Embedder.jl
+++ Embedder.jl
@@ -2,6 +2,7 @@ using BioSequences
 
 function positional_encoding(seq_length,seq_num,d_model::dimension) where {dimension<:Integer}
     encoding = fill(0.0,(d_model,seq_length,seq_num))
+    encoding = convert(Array{Float32, seq_num},encoding)
     for batch in 1:seq_num
         for pos in batch
             for i in 1:2:(d_model-1)
